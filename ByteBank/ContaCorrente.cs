6cea81fb39577af793c18d5cb19064323e80c2c9
renanribs <renanrbrs@gmail.com>
2022-08-20 15:45:21 -0300

    Update

--- ByteBank/ContaCorrente.cs
+++ ByteBank/ContaCorrente.cs
@@ -13,5 +13,22 @@ namespace ByteBank
         public int numero_agencia;
         public string nome_agencia;
         public double saldo;
+        
+        public bool Sacar(double valor)
+        {
+            if(saldo < valor)
+            {
+                return false;
+            } else
+            {
+                saldo = saldo - valor;
+                return true;
+            }
+        }
+
+        public void Depositar (double valor)
+        {
+            saldo = saldo + valor;
+        }
     }
 }
--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -10,8 +10,27 @@ conta1.numero_agencia = 26;
 conta1.nome_agencia = "Agencia Central";
 conta1.saldo = 100;
 
-Console.WriteLine($"Titular: {conta1.titular}");
-Console.WriteLine($"Conta: {conta1.conta}");
-Console.WriteLine($"Numero da agencia: {conta1.numero_agencia}");
-Console.WriteLine($"Nome da agencia: {conta1.nome_agencia}");
-Console.WriteLine($"Saldo: {conta1.saldo}");
+ContaCorrente conta2 = new ContaCorrente();
+conta2.titular = "Vanessa Maluf";
+conta2.conta = "10122-x";
+conta2.numero_agencia = 26;
+conta2.nome_agencia = "Agencia Central";
+conta2.saldo = 100;
+
+Console.WriteLine($"Saldo Renan: {conta1.saldo}");
+
+bool sacar = conta1.Sacar(500);
+
+if (sacar)
+{
+    Console.WriteLine("Saque Realizado com sucesso!");
+}
+else { Console.WriteLine("Saque não pode ser realizado."); }
+
+Console.WriteLine($"Saldo Renan após saque: {conta1.saldo}");
+Console.WriteLine($"Saldo Vanessa: {conta2.saldo}");
+Console.WriteLine("Depositando na conta Vanessa");
+conta2.Depositar(50);
+Console.WriteLine($"Saldo Vanessa: {conta2.saldo}");
+
+Console.WriteLine();
